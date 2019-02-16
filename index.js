74a02df7f6e48076cfa9ad0304df6eb686aa2be6
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-16 16:58:03 -0500

    practice callbacks and promises

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
  * *************
  * 
  */
+
+const call = require("./src/call");
+
+// console.log(call.sync('Jose Daniel'));
+// call.withCallback('Emilio Rojas', call.sync);
+
+call.withPromise("Emilio Rojas")
+  .then(name => { console.log(name) });
