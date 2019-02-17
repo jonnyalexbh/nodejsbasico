32c29f5a687968886f1663608150b988d406f089
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-17 16:14:23 -0500

    practice data entry by terminal

--- index.js
+++ index.js
@@ -5,3 +5,41 @@
  * *************
  * 
  */
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const q = [
+  "Cual es tu primer nombre? ",
+  "Cual es tu primer apellido? ",
+  "Cual es tu edad? "
+];
+
+const AskQuestion = (rl, question) => {
+  return new Promise((resolve, reject) => {
+    rl.question(question, answer => {
+      resolve(answer)
+    });
+  })
+}
+
+const Ask = function (questions) {
+  return new Promise(async resolve => {
+    let results = [];
+    for (let i = 0; i < questions.length; i += 1) {
+      const result = await AskQuestion(rl, questions[i])
+      results.push(result)
+    }
+    rl.close();
+    resolve(results);
+  })
+}
+
+Ask(q)
+  .then(q => {
+    console.log(`Hola ${q[0]} ${q[1]}, tu edad es ${q[2]}`)
+  });
