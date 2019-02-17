5a9017c204c2c9b435ed8b2e137fbc53e3bc5a1c
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-17 12:10:46 -0500

    practice file system

--- index.js
+++ index.js
@@ -5,3 +5,33 @@
  * *************
  * 
  */
+
+const fs = require("fs");
+const ops = require("./src/fileops");
+
+let incValue;
+let greeting;
+
+fs.readFile("./resources/number.txt", "utf8", (err, text) => {
+  if (err) throw err;
+  const numbers = text.split("\n").map(n => Number(n));
+  incValue = ops.incrementValues(numbers)
+
+  fs.writeFile("./resources/numberNew.txt", incValue.join("\n"), (err, result) => {
+    if (err) throw err;
+  })
+
+});
+
+
+fs.readFile("./resources/name.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  const names = data.split("\n").map(n => n);
+  greeting = ops.callNames(names)
+
+  fs.writeFile("./resources/greetingNew.txt", greeting.join("\n"), (err, result) => {
+    if (err) throw err;
+  })
+})
+
+
