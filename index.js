239d8053777f6eb4fb93b0b0eb812711f6beedea
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-17 11:13:28 -0500

    practice async and await

--- index.js
+++ index.js
@@ -5,3 +5,17 @@
  * *************
  * 
  */
+
+const call = require("./src/call");
+
+// call.greet('jonnyalexbh');
+
+async function callWithPromise() {
+  const fullName = await call.withPromise('Jonny', 'Lopez')
+  console.log(fullName);
+}
+
+callWithPromise()
+
+// call.withPromise('Jonny', 'Lopez')
+//   .then(n => console.log(n));
