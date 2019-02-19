14f096d9cc419069f618c229beef018e5bccf485
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-19 07:54:37 -0500

    practice child process

--- index.js
+++ index.js
@@ -5,3 +5,18 @@
  * *************
  *  
  */
+
+const cp = require("child_process");
+
+function execCommand(command) {
+  cp.exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.log(`Error: ${err}`); return;
+    }
+    if (stdout) console.log(`Standard out: \n ${stdout}`);
+    if (stderr) console.log(`Standard err: \n ${stderr}`);
+  });
+}
+
+// execCommand("ls");
+execCommand("node ./src/script --base=5");
