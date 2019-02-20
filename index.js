4d95fd82eaf9c04d9206636d5b470b42d6ec7fbf
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-20 18:32:51 -0500

    practice errors

--- index.js
+++ index.js
@@ -5,3 +5,14 @@
  * *************
  * 
  */
+
+const errors = require("./src/errors");
+
+try {
+  errors.standardErr.range();
+}
+catch (err) {
+  console.log("Ha ocurrido un error: ", err);
+}
+
+console.log('final');
