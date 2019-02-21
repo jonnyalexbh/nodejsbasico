d5d314c73df8ce73ddc870527da1d1849c56f6ba
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-20 19:03:20 -0500

    practice events

--- index.js
+++ index.js
@@ -5,3 +5,12 @@
  * *************
  * 
  */
+
+const greet = require("./src/greet");
+
+// greet.emit('clap');
+// greet.emit('shout', 'VIVA LA WEB');
+
+greet.emit('call', 'jonnyalexbh', (name) => {
+  console.log("Estamos llamando a: ", name);
+})
