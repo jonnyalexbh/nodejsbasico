19f3207b9672f1e0667774e165d56d17ce0bff04
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-23 13:37:01 -0500

    practice building an API with Express.js

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const { PORT } = require("./config");
 
-require("./routes/views")(app);
 require("./routes/api")(app);
+require("./routes/views")(app);
 
-function init(){
-    console.log("Iniciando instancia de Express...");
-    app.listen(PORT, ()=>{
-        console.log("El servidor Express esta activo.");
-    });
+function init() {
+  console.log("Iniciando instancia de Express...");
+  app.listen(PORT, () => {
+    console.log("El servidor Express esta activo.");
+  });
 }
 
 init();
