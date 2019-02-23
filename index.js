96aa27d876bc54cb2bd1de3ba4fbe458f6ab5810
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-23 12:46:04 -0500

    practice creating a web server with Express.js

--- index.js
+++ index.js
@@ -5,3 +5,22 @@
  * *************
  * 
  */
+
+const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Estoy en la ruta Home');
+});
+
+app.get('/app', (req, res) => {
+  res.send('Estoy en la ruta /App');
+});
+
+app.get('*', (req, res) => {
+  res.send('No se donde estoy');
+});
+
+app.listen(3000, () => {
+  console.log('El servidor Express esta en escucha en el puerto 3000');
+});
