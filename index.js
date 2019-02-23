756b6bd271e7542e80362bec1ce88013443f8e53
jonnyalexbh <jonnyalex.bh@gmail.com>
2019-02-23 12:10:29 -0500

    practice creating a basic web server with HTTP

--- index.js
+++ index.js
@@ -5,3 +5,16 @@
  * *************
  *  
  */
+
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  fs.readFile("./resources/my_page.html", (err, data) => {
+    if (err) { console.error(err); return; }
+    res.end(data)
+  })
+})
+
+console.log('Iniciando server ...');
+server.listen(3000)
